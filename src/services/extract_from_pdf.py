9971e8fa520a1dc43d258e31cadf06f9bb5b5292
Abhay Koka <abhay.koka@winwire.com>
2026-09-06 12:32:31 +0530

    extraction working improved a bit

--- src/services/extract_from_pdf.py
+++ src/services/extract_from_pdf.py
@@ -1,4 +1,15 @@
 from unstructured.partition.auto import partition
+filepath="C://Users//AbhayKoka//ragap//src//Policy_Documents//sample.pdf"
 
-elements = partition(filename="C://Users//AbhayKoka//ragap//src//Policy_Documents//sample.pdf")
-print("\n\n".join([str(el) for el in elements]))
+def docextract(filepath):
+    elements = partition(filename=filepath)
+    print(type(elements))
+    
+    #print([str(el) for el in elements])
+    e=[str(el) for el in elements]
+    print(e)
+    #print("\n\n".join([str(el) for el in elements]))
+    
+    return e
+
+docextract(filepath)
